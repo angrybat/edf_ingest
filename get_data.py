b95157dfb9712ff7c886473b9d7708f750a96584
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-13 12:51:51 +0000

    Changed get_data to use the new Variables class

--- get_data.py
+++ get_data.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from json import load
 from pathlib import Path
 
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
+
+from src.models import GasFilter, ReadingFrequencyType, UtilityFilter, Variables
 
 # get sensitive data from env file
 env_file_path = Path("env.json")
@@ -19,21 +22,26 @@ transport = AIOHTTPTransport(url=URL, headers=HEADERS)
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
 # Define variables and query
-params = {
-    "accountNumber": settings["account_number"],
-    "startAt": "2024-01-11T00:00:00.000Z",
-    "endAt": "2024-01-12T00:00:00.000Z",
-    "first": 48,
-    "utilityFilters": [
-        {"electricityFilters": {"readingFrequencyType": "HOUR_INTERVAL"}}
+variables = Variables(
+    account_number=settings["account_number"],
+    start_at=datetime(year=2024, month=1, day=11),
+    end_at=datetime(year=2024, month=1, day=12),
+    first=48,
+    utility_filters=[
+        GasFilter(
+            gas_filters=UtilityFilter(
+                reading_frequency_type=ReadingFrequencyType.HOUR_INTERVAL
+            )
+        )
     ],
-}
+)
+
 query = gql(
     "query getMeasurements($accountNumber: String!, $first: Int!, $utilityFilters: [UtilityFiltersInput!], $startAt: DateTime, $endAt: DateTime) {\n  account(accountNumber: $accountNumber) {\n    properties {\n      measurements(\n        first: $first\n        utilityFilters: $utilityFilters\n        startAt: $startAt\n        endAt: $endAt\n      ) {\n        edges {\n          node {\n            value\n            ... on IntervalMeasurementType {\n              startAt\n              endAt\n              value\n              metaData {\n                statistics {\n                  type\n                  costInclTax {\n                    estimatedAmount\n                    __typename\n                  }\n                  __typename\n                }\n                __typename\n              }\n              __typename\n            }\n            metaData {\n              utilityFilters {\n                __typename\n              }\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}"  # noqa: E501
 )
 
 # get data
-result = client.execute(query, variable_values=params)
+result = client.execute(query, variable_values=variables.model_dump(by_alias=True))
 
 # print data
 print(result)
