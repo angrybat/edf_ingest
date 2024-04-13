ed5df8589dc8590bbf29900e0e43e44fb3029f5d
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-13 12:43:36 +0000

    Fixed failing tests

--- src/models.py
+++ src/models.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from enum import Enum
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, field_serializer
+from pydantic.alias_generators import to_camel
 
 
 class ReadingFrequencyType(Enum):
@@ -15,20 +16,36 @@ class ReadingFrequencyType(Enum):
 
 
 class UtilityFilter(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
     reading_frequency_type: ReadingFrequencyType
+
+    @field_serializer("reading_frequency_type", return_type=str)
+    def get_enum_string(self, reading_frequency_type: ReadingFrequencyType) -> str:
+        return reading_frequency_type.value
 
 
 class GasFilter(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
     gas_filters: UtilityFilter
 
 
 class ElectricityFilter(BaseModel):
-    electricity_filter: UtilityFilter
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+    electricity_filters: UtilityFilter
 
 
 class Variables(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
     account_number: str
     start_at: datetime
     end_at: datetime
     first: int
     utility_filters: List[GasFilter | ElectricityFilter]
+
+    @field_serializer(
+        "start_at",
+        "end_at",
+        return_type=str,
+    )
+    def get_datetime_str(self, datetime: datetime) -> str:
+        return datetime.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
