1f43cc4b2358d53ceda81bc49521e153dcc0eb86
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 15:22:23 +0000

    Refactored to add a queries folder constant

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-GET_READINGS_QUERY_FILE_PATH = Path("src/queries/get_readings.graphql")
-ACCOUNT_NUMBER_QUERY_FILE_PATH = Path("src/queries/get_account_number.graphql")
+QUERIES_FOLDER = Path("src/queries")
+GET_READINGS_QUERY_FILE_PATH = QUERIES_FOLDER / "get_readings.graphql"
+ACCOUNT_NUMBER_QUERY_FILE_PATH = QUERIES_FOLDER / "get_account_number.graphql"
 ENV_FILE_PATH = Path("env.json")
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
