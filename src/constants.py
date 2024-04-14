cbb410ec5f47c964e0068966f3d6a230590f844a
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 15:19:31 +0000

    Moved queries to a separate folder

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-GET_READINGS_QUERY_FILE_PATH = Path("src/get_readings.graphql")
-ACCOUNT_NUMBER_QUERY_FILE_PATH = Path("src/get_account_number.graphql")
+GET_READINGS_QUERY_FILE_PATH = Path("src/queries/get_readings.graphql")
+ACCOUNT_NUMBER_QUERY_FILE_PATH = Path("src/queries/get_account_number.graphql")
 ENV_FILE_PATH = Path("env.json")
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
