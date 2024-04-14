458819faa6e09a0cf0b399092eca76c532c0f2d7
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 11:23:11 +0000

    Refactored to create a fixture for the ReadingsCursor

--- tests/unit/test_cursors.py
+++ tests/unit/test_cursors.py
@@ -3,6 +3,7 @@ from typing import List
 from unittest.mock import patch
 
 import pytest
+from pytest import fixture
 
 from src.cursors import ReadingsCursor
 from src.models import PaginatedReadings, Reading, Settings
@@ -18,6 +19,22 @@ from tests.unit.constants import (
 )
 
 
+@fixture
+def readings_cursor() -> ReadingsCursor:
+    query_file_path = Path("src/get_measurements.graphql")
+    settings = Settings(
+        account_number=ACCOUNT_NUMBER,
+        jwt=JWT,
+        url=URL,
+        start_at=START_AT,
+        end_at=END_AT,
+        first=FIRST,
+        gas_reading_frequency=GAS_READING_FREQUENCY,
+        electricity_reading_frequency=ELECTRICITY_READING_FREQUENCY,
+    )
+    return ReadingsCursor(settings, query_file_path)
+
+
 class TestReadingsCursor:
     @patch("src.cursors.get_paginated_readings")
     @pytest.mark.parametrize(
@@ -25,71 +42,39 @@ class TestReadingsCursor:
         [True, False],
         ids=["has_next_page_is_true", "has_next_page_is_false"],
     )
-    def test_next_page_return_value(self, mock_method, expected: bool) -> None:
+    def test_next_page_return_value(
+        self, mock_method, readings_cursor: ReadingsCursor, expected: bool
+    ) -> None:
         mock_method.return_value = PaginatedReadings(
             readings=[], has_next_page=expected
         )
-        query_file_path = Path("src/get_measurements.graphql")
-        settings = Settings(
-            account_number=ACCOUNT_NUMBER,
-            jwt=JWT,
-            url=URL,
-            start_at=START_AT,
-            end_at=END_AT,
-            first=FIRST,
-            gas_reading_frequency=GAS_READING_FREQUENCY,
-            electricity_reading_frequency=ELECTRICITY_READING_FREQUENCY,
-        )
-        reading_cursor = ReadingsCursor(settings, query_file_path)
 
-        assert expected == reading_cursor.next_page()
+        assert expected == readings_cursor.next_page()
 
     @patch("src.cursors.get_paginated_readings")
     def test_next_page_updates_gas_readings(
         self,
         mock_method,
+        readings_cursor: ReadingsCursor,
         paginated_readings: PaginatedReadings,
         gas_readings: List[Reading],
     ) -> None:
         mock_method.return_value = paginated_readings
-        query_file_path = Path("src/get_measurements.graphql")
-        settings = Settings(
-            account_number=ACCOUNT_NUMBER,
-            jwt=JWT,
-            url=URL,
-            start_at=START_AT,
-            end_at=END_AT,
-            first=FIRST,
-            gas_reading_frequency=GAS_READING_FREQUENCY,
-            electricity_reading_frequency=ELECTRICITY_READING_FREQUENCY,
-        )
-        reading_cursor = ReadingsCursor(settings, query_file_path)
 
-        reading_cursor.next_page()
+        readings_cursor.next_page()
 
-        assert gas_readings == reading_cursor.gas_readings
+        assert gas_readings == readings_cursor.gas_readings
 
     @patch("src.cursors.get_paginated_readings")
     def test_next_page_updates_electricity_readings(
         self,
         mock_method,
+        readings_cursor: ReadingsCursor,
         paginated_readings: PaginatedReadings,
         electricity_readings: List[Reading],
     ) -> None:
         mock_method.return_value = paginated_readings
-        query_file_path = Path("src/get_measurements.graphql")
-        settings = Settings(
-            account_number=ACCOUNT_NUMBER,
-            jwt=JWT,
-            url=URL,
-            start_at=START_AT,
-            end_at=END_AT,
-            first=FIRST,
-            gas_reading_frequency=GAS_READING_FREQUENCY,
-            electricity_reading_frequency=ELECTRICITY_READING_FREQUENCY,
-        )
-        reading_cursor = ReadingsCursor(settings, query_file_path)
 
-        reading_cursor.next_page()
+        readings_cursor.next_page()
 
-        assert electricity_readings == reading_cursor.electricity_readings
+        assert electricity_readings == readings_cursor.electricity_readings
