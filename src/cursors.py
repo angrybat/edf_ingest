083b2624ac25da92e637f0914034e043c06f6f10
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 12:00:07 +0000

    Fixed failing test

--- src/cursors.py
+++ src/cursors.py
@@ -7,17 +7,23 @@ from src.models import PaginatedReadings, Reading, Settings
 
 class ReadingsCursor:
     def __init__(self, settings: Settings) -> None:
-        self.settings = settings
+        self.variables = get_variables(settings)
+        self.url = settings.url
+        self.jwt = settings.jwt
+        self.query_file_path = settings.query_file_path
         self._paginated_readings: PaginatedReadings | None = None
 
     def next_page(self) -> bool:
-        variables = get_variables(self.settings)
         self._paginated_readings = get_paginated_readings(
-            self.settings.url,
-            self.settings.jwt,
-            self.settings.query_file_path,
-            variables,
+            self.url,
+            self.jwt,
+            self.query_file_path,
+            self.variables,
         )
+        self.variables.start_at = sorted(
+            [reading.end_at for reading in self._paginated_readings.readings],
+            reverse=True,
+        )[0]
         return self._paginated_readings.has_next_page
 
     @property
