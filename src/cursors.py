7ca40c6cea73431b785e8164de60efd0ee19c5ae
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 13:41:58 +0000

    Fixed failing test

--- src/cursors.py
+++ src/cursors.py
@@ -20,12 +20,8 @@ class ReadingsCursor:
             self.query_file_path,
             self.variables,
         )
-        self.variables.start_at = self._get_next_start_at()
+        self.variables.after = self._paginated_readings.cursor
         return self._paginated_readings.has_next_page
-
-    def _get_next_start_at(self):
-        end_ats = [reading.end_at for reading in self._paginated_readings.readings]
-        return sorted(end_ats, reverse=True)[0]
 
     @property
     def gas_readings(self) -> List[Reading]:
