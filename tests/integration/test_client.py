222afcb6a55b7a00346db8f8a05ea8855ae10268
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 20:01:03 +0000

    Added test to check that a JWT can be retrieved

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-from src.client import get_account_number, get_paginated_readings
+from src.client import get_account_number, get_credentials, get_paginated_readings
 from src.constants import (
     ACCOUNT_NUMBER_QUERY_FILE_PATH,
     ENV_FILE_PATH,
     GET_READINGS_QUERY_FILE_PATH,
 )
 from src.factories import get_readings_variables, get_settings
-from src.models import PaginatedReadings
+from src.models import Credentials, PaginatedReadings
 
 
 def test_can_retrieve_paginated_readings() -> None:
@@ -22,3 +22,14 @@ def test_can_retrieve_paginated_readings() -> None:
     )
 
     assert isinstance(paginated_readings, PaginatedReadings)
+
+
+def test_can_retrieve_credentials() -> None:
+    env_file_path = Path(ENV_FILE_PATH)
+    settings = get_settings(env_file_path)
+
+    credentials = get_credentials(
+        settings.url, settings.email_address, settings.password
+    )
+
+    assert isinstance(credentials, Credentials)
