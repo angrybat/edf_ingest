f5f3b0054dba338530b25de42f194a266f8914d3
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 14:45:50 +0000

    Added failing test for the method get_account_number to check it does not return and empty string

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
 QUERY_FILE_PATH = Path("src/get_measurements.graphql")
+ACCOUNT_NAME_QUERY_FILE_PATH = Path("src/get_account_number.graphql")
 ENV_FILE_PATH = Path("env.json")
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-from src.client import get_paginated_readings
-from src.constants import QUERY_FILE_PATH
+from src.client import get_account_name, get_paginated_readings
+from src.constants import ACCOUNT_NAME_QUERY_FILE_PATH, QUERY_FILE_PATH
 from src.factories import get_settings, get_variables
 from src.models import PaginatedReadings
 
 
-def test_api_call_does_not_throw_error():
+def test_api_call_does_not_throw_error() -> None:
     env_file_path = Path("env.json")
     settings = get_settings(env_file_path)
     variables = get_variables(settings)
@@ -15,3 +15,13 @@ def test_api_call_does_not_throw_error():
     )
 
     assert isinstance(paginated_readings, PaginatedReadings)
+
+
+def test_get_account_name_does_not_return_an_empty_string() -> None:
+    env_file_path = Path("env.json")
+    settings = get_settings(env_file_path)
+    account_name = get_account_name(
+        settings.url, settings.jwt, ACCOUNT_NAME_QUERY_FILE_PATH
+    )
+
+    assert "" != account_name
