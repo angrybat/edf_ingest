ab3cb3e84e26d3b46dcd92af1628f1249a4ed14b
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 15:14:50 +0000

    Used the ENV_FILE_PATH constant

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 
 from src.client import get_account_number, get_paginated_readings
-from src.constants import ACCOUNT_NUMBER_QUERY_FILE_PATH, GET_READINGS_QUERY_FILE_PATH
+from src.constants import (
+    ACCOUNT_NUMBER_QUERY_FILE_PATH,
+    ENV_FILE_PATH,
+    GET_READINGS_QUERY_FILE_PATH,
+)
 from src.factories import get_settings, get_variables
 from src.models import PaginatedReadings
 
 
 def test_can_retrieve_paginated_readings() -> None:
-    env_file_path = Path("env.json")
+    env_file_path = Path(ENV_FILE_PATH)
     settings = get_settings(env_file_path)
     account_number = get_account_number(
         settings.url, settings.jwt, ACCOUNT_NUMBER_QUERY_FILE_PATH
