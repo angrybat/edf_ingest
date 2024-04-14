5ff89e83a732d277784eb6bbeabdd43450ea4cf4
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 11:18:58 +0000

    Fixed failing tests

--- src/cursors.py
+++ src/cursors.py
@@ -1,18 +1,28 @@
 from pathlib import Path
+from typing import List
 
 from src.client import get_paginated_readings
 from src.factories import get_variables
-from src.models import Settings
+from src.models import PaginatedReadings, Reading, Settings
 
 
 class ReadingsCursor:
     def __init__(self, settings: Settings, query_file_path: Path) -> None:
         self.settings = settings
         self.query_file_path = query_file_path
+        self._paginated_readings: PaginatedReadings | None = None
 
     def next_page(self) -> bool:
         variables = get_variables(self.settings)
-        paginated_readings = get_paginated_readings(
+        self._paginated_readings = get_paginated_readings(
             self.settings.url, self.settings.jwt, self.query_file_path, variables
         )
-        return paginated_readings.has_next_page
+        return self._paginated_readings.has_next_page
+
+    @property
+    def gas_readings(self) -> List[Reading]:
+        return self._paginated_readings.gas
+
+    @property
+    def electricity_readings(self) -> List[Reading]:
+        return self._paginated_readings.electricity
