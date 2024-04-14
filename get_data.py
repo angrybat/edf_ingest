028c469b15038d0b974654facdaad2289cbf9711
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 14:59:24 +0000

    Changed get_data to get the account_number from the API

--- get_data.py
+++ get_data.py
@@ -1,9 +1,13 @@
-from src.constants import ENV_FILE_PATH
+from src.client import get_account_name
+from src.constants import ACCOUNT_NAME_QUERY_FILE_PATH, ENV_FILE_PATH
 from src.cursors import ReadingsCursor
 from src.factories import get_settings
 
 settings = get_settings(ENV_FILE_PATH)
-cursor = ReadingsCursor(settings)
+account_number = get_account_name(
+    settings.url, settings.jwt, ACCOUNT_NAME_QUERY_FILE_PATH
+)
+cursor = ReadingsCursor(settings, account_number)
 
 gas_readings = []
 electricity_readings = []
