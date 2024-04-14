8232769da3757174743c7f3358a89fd023d790a4
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-14 10:14:05 +0000

    Refactored the get_paginated_readings method to accept variables to give more controls rather than relying on a settings files

--- src/client.py
+++ src/client.py
@@ -1,26 +1,16 @@
 from pathlib import Path
 
-from src.factories import (
-    get_authorized_client,
-    get_query,
-    get_settings,
-    get_utility_filters,
-)
+from src.factories import get_authorized_client, get_query
 from src.models import PaginatedReadings, Variables
 
 
 def get_paginated_readings(
-    settings_file_path: Path, query_file_path: Path
+    url: str,
+    jwt: str,
+    query_file_path: Path,
+    variables: Variables,
 ) -> PaginatedReadings:
-    settings = get_settings(settings_file_path)
-    variables = Variables(
-        account_number=settings.account_number,
-        start_at=settings.start_at,
-        end_at=settings.end_at,
-        first=settings.first,
-        utility_filters=get_utility_filters(settings),
-    )
-    client = get_authorized_client(jwt=settings.jwt, url=settings.url)
+    client = get_authorized_client(jwt, url)
     query = get_query(query_file_path)
     response = client.execute(
         query, variable_values=variables.model_dump(by_alias=True)
--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 
-import pytest
-
 from src.client import get_paginated_readings
-from src.models import PaginatedReadings
+from src.factories import get_settings, get_utility_filters
+from src.models import PaginatedReadings, Variables
 
 
 def test_api_call_does_not_throw_error():
     env_file_path = Path("env.json")
     query_file_path = Path("src/get_measurements.graphql")
-
-    paginated_readings = get_paginated_readings(env_file_path, query_file_path)
+    settings = get_settings(env_file_path)
+    variables = Variables(
+        account_number=settings.account_number,
+        start_at=settings.start_at,
+        end_at=settings.end_at,
+        first=settings.first,
+        utility_filters=get_utility_filters(settings),
+    )
+    paginated_readings = get_paginated_readings(
+        settings.url, settings.jwt, query_file_path, variables
+    )
 
     assert isinstance(paginated_readings, PaginatedReadings)
