a5cf18b73368d2cdf319f2b5e463c6f4eb2c2404
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-16 22:37:05 +0000

    Refactored by extracting methods

--- src/cursors.py
+++ src/cursors.py
@@ -26,18 +26,7 @@ class ReadingsCursor:
         self._paginated_readings: PaginatedReadings | None = None
 
     def next_page(self) -> bool:
-        if (
-            datetime.now(tz=timezone.utc) > self.authorization_tokens.expires_at
-            and datetime.now(tz=timezone.utc)
-            > self.authorization_tokens.refresh_expires_in
-        ):
-            self.authorization_tokens = get_authorization_tokens(
-                self.url, self.email_address, self.password
-            )
-        if datetime.now(tz=timezone.utc) > self.authorization_tokens.expires_at:
-            self.authorization_tokens = refresh_authorization_tokens(
-                self.url, self.authorization_tokens.refresh_expires_in
-            )
+        self._refresh_authorization_tokens()
         self._paginated_readings = get_paginated_readings(
             self.url,
             self.authorization_tokens.jwt,
@@ -46,6 +35,26 @@ class ReadingsCursor:
         )
         self.variables.after = self._paginated_readings.cursor
         return self._paginated_readings.has_next_page
+
+    def _refresh_authorization_tokens(self):
+        if self._jwt_and_refresh_tokens_has_expired():
+            self.authorization_tokens = get_authorization_tokens(
+                self.url, self.email_address, self.password
+            )
+        if self._jwt_has_expired():
+            self.authorization_tokens = refresh_authorization_tokens(
+                self.url, self.authorization_tokens.refresh_expires_in
+            )
+
+    def _jwt_has_expired(self):
+        return datetime.now(tz=timezone.utc) > self.authorization_tokens.expires_at
+
+    def _jwt_and_refresh_tokens_has_expired(self):
+        return (
+            datetime.now(tz=timezone.utc) > self.authorization_tokens.expires_at
+            and datetime.now(tz=timezone.utc)
+            > self.authorization_tokens.refresh_expires_in
+        )
 
     @property
     def gas_readings(self) -> List[Reading]:
