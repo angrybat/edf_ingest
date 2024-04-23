3cee674e933cfa57d566624e2918dcc432a1f499
angrybat <48884025+angrybat@users.noreply.github.com>
2024-04-23 18:54:22 +0000

    Fixed failing test

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from src.client import get_account_number, get_authorization_tokens
 from src.cursors import ReadingsCursor
 from src.factories import get_settings
+from src.models import Readings
 
 
 def get_readings_cursor(settings_file_path: Path) -> Path:
@@ -19,3 +20,13 @@ def get_readings_cursor(settings_file_path: Path) -> Path:
         settings.get_account_number_query_file_path,
     )
     return ReadingsCursor(settings, account_number, authorization_tokens)
+
+
+def get_readings(readings_cursor: ReadingsCursor) -> Readings:
+    readings = Readings(gas=[], electricity=[])
+    while readings_cursor.next_page():
+        readings.gas += readings_cursor.gas_readings
+        readings.electricity += readings_cursor.electricity_readings
+    readings.gas += readings_cursor.gas_readings
+    readings.electricity += readings_cursor.electricity_readings
+    return readings
